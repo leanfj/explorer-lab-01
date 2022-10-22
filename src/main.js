e680e77a334b5cba515567e7a1cb2487dd34f1c3
Leandro Ferreira <leanfj@hotmail.com>
2022-10-22 10:29:29 -0300

    Adicionando estilo para error nos ipnuts

--- src/main.js
+++ src/main.js
@@ -10,51 +10,16 @@ const colors = {
 const bgColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const bgColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const logoCartao = document.querySelector(".cc-logo span:nth-child(2) img")
-
-const cvc = document.getElementById("security-code")
-const cvcPattern = {
-  mask: "000",
-  minLenght: 3,
-  placeholderChar: "0",
-}
-
-const cvcMasked = IMask(cvc, cvcPattern)
-
-// cvc.addEventListener("input", () => {
-//   const cvcNumber = document.querySelector(".cc-security .value")
-//   cvcNumber.textContent = cvc.value.length > 0 ? cvc.value : "123"
-// })
-
-//Utilizando o evento input com Imasked para capturar o valor do input
-cvcMasked.on("accept", () => {
-  const cvcNumber = document.querySelector(".cc-security .value")
-  cvcNumber.textContent = cvc.value.length > 0 ? cvcMasked.value : "123"
-})
-
-const expirationDate = document.getElementById("expiration-date")
-const expirationDatePattern = {
-  mask: "MM{/}YY",
-  blocks: {
-    MM: {
-      mask: IMask.MaskedRange,
-      from: 1,
-      to: 12,
-    },
-    YY: {
-      mask: IMask.MaskedRange,
-      from: String(new Date().getFullYear()).slice(2),
-      to: String(new Date().getFullYear() + 10).slice(2),
-    }
-  }
-}
-const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
-
-expirationDate.addEventListener("input", (e) => {
-  const ccExpiration = document.querySelector(".cc-expiration .value")
-  ccExpiration.innerText = expirationDate.value.length > 0 ? expirationDate.value : "02/32"
-})
-
+const cardHolder = document.getElementById("card-holder")
 const cardNumberInput = document.getElementById("card-number")
+const cvc = document.getElementById("security-code")
+const expirationDate = document.getElementById("expiration-date")
+const sendButton = document.getElementById("send-button")
+const formElement = document.querySelector("form")
+
+
+
+
 const cardNumberPattern = {
   mask: [
     {
@@ -91,6 +56,110 @@ cardNumberInput.addEventListener("input", (e) => {
   ccNumber.innerText = cardNumberInput.value.length > 0 ? cardNumberInput.value : "1234 5678 9012 3456"
 })
 
+cardNumberInput.addEventListener("focusout", (e) => {
+  if (cardNumberInput.value.length < 19) {
+    cardNumberInput.classList.add("error")
+  } else {
+    cardNumberInput.classList.remove("error")
+  }
+})
+
+cardNumberInput.addEventListener("focusin", (e) => {
+  if (cardNumberInput.classList.contains("error")) {
+    cardNumberInput.classList.remove("error")
+  }
+})
+
+cardHolder.addEventListener("input", (e) => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+  ccHolder.innerText = cardHolder.value.length > 0 ? cardHolder.value : "Fulano da Silva"
+})
+
+cardHolder.addEventListener("focusout", (e) => {
+  if (cardHolder.value.length === 0) {
+    cardHolder.classList.add("error")
+  } else {
+    cardHolder.classList.remove("error")
+  }
+})
+
+cardHolder.addEventListener("focusin", (e) => {
+  if (cardHolder.classList.contains("error")) {
+    cardHolder.classList.remove("error")
+  }
+})
+
+const cvcPattern = {
+  mask: "000",
+  minLenght: 3,
+  placeholderChar: "0",
+}
+
+const cvcMasked = IMask(cvc, cvcPattern)
+
+// cvc.addEventListener("input", () => {
+//   const cvcNumber = document.querySelector(".cc-security .value")
+//   cvcNumber.textContent = cvc.value.length > 0 ? cvc.value : "123"
+// })
+
+//Utilizando o evento input com Imasked para capturar o valor do input
+cvcMasked.on("accept", () => {
+  const cvcNumber = document.querySelector(".cc-security .value")
+  cvcNumber.textContent = cvc.value.length > 0 ? cvcMasked.value : "123"
+})
+
+cvc.addEventListener("focusout", (e) => {
+  if (cvc.value.length < 3) {
+    cvc.classList.add("error")
+  } else {
+    cvc.classList.remove("error")
+  }
+})
+
+cvc.addEventListener("focusin", (e) => {
+  if (cvc.classList.contains("error")) {
+    cvc.classList.remove("error")
+  }
+})
+
+
+
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    }
+  }
+}
+
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+expirationDate.addEventListener("input", (e) => {
+  const ccExpiration = document.querySelector(".cc-expiration .value")
+  ccExpiration.innerText = expirationDate.value.length > 0 ? expirationDate.value : "02/32"
+})
+
+expirationDate.addEventListener("focusout", (e) => {
+  if (expirationDate.value.length < 5) {
+    expirationDate.classList.add("error")
+  } else {
+    expirationDate.classList.remove("error")
+  }
+})
+
+expirationDate.addEventListener("focusin", (e) => {
+  if (expirationDate.classList.contains("error")) {
+    expirationDate.classList.remove("error")
+  }
+})
 
 
 function setCardsType(type) {
@@ -99,13 +168,25 @@ function setCardsType(type) {
   logoCartao.setAttribute("src", colors[type].logo)
 }
 
-const cardHolder = document.getElementById("card-holder")
+sendButton.addEventListener("click", async (e) => {
+  e.preventDefault()
+  const cardNumber = cardNumberMasked.value
+  const cardHolderValue = cardHolder.value.toUpperCase()
+  const cardExpiration = expirationDateMasked.value
+  const cardCvc = cvcMasked.value
+  if (cardNumber.length < 19 || cardHolderValue.length < 3 || cardExpiration.length < 5 || cardCvc.length < 3) {
+    const allInputs = document.querySelectorAll("input")
+    allInputs.forEach((input) => {
+      input.classList.add("error")
+    })
+    alert("Preencha todos os campos corretamente")
 
-cardHolder.addEventListener("input", (e) => {
-  const ccHolder = document.querySelector(".cc-holder .value")
-  ccHolder.innerText = cardHolder.value.length > 0 ? cardHolder.value : "Fulano da Silva"
+  } else {
+    alert("Cartão cadastrado com sucesso")
+    formElement.reset()
+  }
+  console.log(cardNumber, cardHolderValue, cardExpiration, cardCvc)
 })
-
 // setCardsType("masterCard")
 
 // setInterval(() => {
